1e6adb9e5f551e061b637dcb8db2495d5ec45155
Salah <S7baijan@hotmail.com>
2021-09-15 21:29:45 +0300

    setup user service test

--- src/services/test/user.service.spec.ts
+++ src/services/test/user.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaModule } from '../../modules/prisma.module';
 import { UserService } from '../user.service';
 
 describe('UserService', () => {
@@ -6,6 +7,7 @@ describe('UserService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports:[PrismaModule],
       providers: [UserService],
     }).compile();
 
