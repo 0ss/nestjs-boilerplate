8e25d68aa1281159dce0594af3916e1a5d68b588
Salah <S7baijan@hotmail.com>
2021-09-20 00:26:38 +0300

    init project service test

--- src/services/test/project.service.spec.ts
+++ src/services/test/project.service.spec.ts
@@ -1,18 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { createProjectInputFactory, projectFactory } from '../../../test/factories/project.factory';
+import { PrismaModule } from '../../modules/prisma.module';
+import { PrismaService } from '../prisma.service';
 import { ProjectService } from '../project.service';
 
 describe('ProjectService', () => {
-  let service: ProjectService;
-
+  let projectService: ProjectService;
+  let prismaService : PrismaService
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [ProjectService],
     }).compile();
 
-    service = module.get<ProjectService>(ProjectService);
+    projectService = module.get<ProjectService>(ProjectService);
+    prismaService = module.get<PrismaService>(PrismaService);
+
+
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(projectService).toBeDefined();
+  });
+
+  
+  describe('create', () => {
+    it('should create new project', async () => {
+      // const createProjectInput  = createProjectInputFactory.build();
+      // const project = projectFactory.build(createProjectInput).;
+      // jest
+      //   .spyOn(prismaService.project, 'create')
+      //   .mockResolvedValueOnce()
+      // const result = await projectService.create(createProjectInputFactory);
+      // expect(result).toEqual(project);
+    });
   });
 });
