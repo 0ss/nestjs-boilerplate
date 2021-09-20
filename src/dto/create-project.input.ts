5653711de9a49f3b3ac881d4b28e53b2c4a8c6e3
Salah <S7baijan@hotmail.com>
2021-09-20 22:51:51 +0300

    add min length to name project input

--- src/dto/create-project.input.ts
+++ src/dto/create-project.input.ts
@@ -1,7 +1,10 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { MinLength } from 'class-validator';
 
 @InputType()
 export class CreateProjectInput {
+
   @Field()
+  @MinLength(1)
   name: string;
 }
