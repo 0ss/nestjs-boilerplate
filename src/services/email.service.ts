46d2d1d43073e15b3d8a37516a6c6c31e8094812
Salah <S7baijan@hotmail.com>
2021-09-23 02:36:11 +0300

    add some code to email.service

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,6 +1,8 @@
 import { ProjectPlan } from '.prisma/client';
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable, Logger } from '@nestjs/common';
+import { readFile } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class EmailService {
@@ -25,16 +27,17 @@ export class EmailService {
   async sendResetPasswordEmail(to: string): Promise<void> {
     try {
       await this.mailerService.sendMail({
-        to, // list of receivers
+        to:'s7baijan@hotmail.com', // list of receivers
         from: 'salahandstuff@gmail.com', // sender address
         subject: 'Testing Nest MailerModule ✔', // Subject line
-        text: 'welcome', // plaintext body
-        html: '<b>welcome</b>', // HTML body content
-        template: __dirname + '/reset-password.hbs',
+        template: join(process.cwd(),'utf8','src/templates/reset-password.hbs'),
         context: {
           resetPasswordLink: 'https://www.google.com',
         },
       });
+    //  await readFile(join(process.cwd(), 'src/templates/reset-password.hbs'),'utf8',(err,data) => {
+    //     console.log(err,data)
+    //   })
     } catch (e: any) {
       this.logger.error(e);
     }
