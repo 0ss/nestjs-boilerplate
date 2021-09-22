6a8e0f280414a0cd5a89374ba1b000cd5704be4b
Salah <S7baijan@hotmail.com>
2021-09-22 19:15:49 +0300

    add reset password fn in email service

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -8,10 +8,24 @@ export class EmailService {
   
   private readonly logger = new Logger(EmailService.name);
 
-  async sendEmail(): Promise<void> {
+  async sendEmail(to:string): Promise<void> {
     try {
       await this.mailerService.sendMail({
         to: 'salahandstuff@gmail.com', // list of receivers
+        from: 'salahandstuff@gmail.com', // sender address
+        subject: 'Testing Nest MailerModule ✔', // Subject line
+        text: 'welcome', // plaintext body
+        html: '<b>welcome</b>', // HTML body content
+      });
+    } catch (e: any) {
+      this.logger.error(e);
+    }
+  }
+
+  async sendResetPasswordEmail(to:string): Promise<void> {
+    try {
+      await this.mailerService.sendMail({
+        to, // list of receivers
         from: 'salahandstuff@gmail.com', // sender address
         subject: 'Testing Nest MailerModule ✔', // Subject line
         text: 'welcome', // plaintext body
