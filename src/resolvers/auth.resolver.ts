148ea0ff20b377c891fb54afc4145b2c09d6e323
Salah <S7baijan@hotmail.com>
2021-09-22 19:06:19 +0300

    add function that create project after a userregister in auth resolver

--- src/resolvers/auth.resolver.ts
+++ src/resolvers/auth.resolver.ts
@@ -1,25 +1,35 @@
 import { Logger } from '@nestjs/common';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { registerUserInputFactory } from '../../test/factories/user.factory';
 import { LoginUserInput } from '../dto/login-user.input';
 import { RegisterSocialInput } from '../dto/register-social.input';
 import { RegisterUserInput } from '../dto/register-user.input';
-import { User } from '../entities/user.entity';
 import { UserToken } from '../entities/usertoken.entity';
 import { AuthService } from '../services/auth.service';
+import { ProjectService } from '../services/project.service';
+import { UserService } from '../services/user.service';
 
 @Resolver()
 export class AuthResolver {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly projectService: ProjectService,
+  ) {}
 
   private readonly logger = new Logger(AuthResolver.name);
-
+  // after user register to the app we will automaticlly create project for them
+  // so they can have a test project
   @Mutation(() => UserToken)
   async register(
     @Args('registerUserInput') registerUserInput: RegisterUserInput,
   ): Promise<UserToken> {
     const user = await this.authService.register(registerUserInput);
     const token = await this.authService.createToken(user);
+    await this.projectService.create(
+      {
+        name: 'Project Test',
+      },
+      user.id,
+    );
     return { token, user };
   }
 
@@ -29,6 +39,12 @@ export class AuthResolver {
   ): Promise<UserToken> {
     const user = await this.authService.registerSocial(registerSocialInput);
     const token = await this.authService.createToken(user);
+    await this.projectService.create(
+      {
+        name: 'Project Test',
+      },
+      user.id,
+    );
     return { token, user };
   }
 
