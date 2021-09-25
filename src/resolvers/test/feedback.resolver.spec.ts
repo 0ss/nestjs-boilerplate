7826a7d2b32a1f25e6e8c513299130222cfd4212
Salah <S7baijan@hotmail.com>
2021-09-25 23:20:25 +0300

    init feedback resolver test

--- src/resolvers/test/feedback.resolver.spec.ts
+++ src/resolvers/test/feedback.resolver.spec.ts
@@ -1,19 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { FeedbackResolver } from '../feedback.resolver';
+import { feedbackFactory } from '../../../test/factories/feedback.factory';
+import { PrismaModule } from '../../modules/prisma.module';
+import { UserModule } from '../../modules/user.module';
 import { FeedbackService } from '../../services/feedback.service';
+import { UserService } from '../../services/user.service';
+import { FeedbackResolver } from '../feedback.resolver';
 
 describe('FeedbackResolver', () => {
-  let resolver: FeedbackResolver;
+  let feedbackResolver: FeedbackResolver;
+  let feedbackService: FeedbackService;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FeedbackResolver, FeedbackService],
+      imports:[UserModule,PrismaModule],
+      providers: [FeedbackResolver, FeedbackService, ],
     }).compile();
 
-    resolver = module.get<FeedbackResolver>(FeedbackResolver);
+    feedbackResolver = module.get<FeedbackResolver>(FeedbackResolver);
+    feedbackService = module.get<FeedbackService>(FeedbackService);
+    userService = module.get<UserService>(UserService);
+
   });
 
   it('should be defined', () => {
-    expect(resolver).toBeDefined();
+    expect(feedbackResolver).toBeDefined();
   });
+ 
 });
