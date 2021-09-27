fea8dd6c8534f42691bed242c92a3c38d95d789d
Salah <S7baijan@hotmail.com>
2021-09-27 23:16:02 +0300

    finish project resolver spec

--- src/resolvers/test/project.resolver.spec.ts
+++ src/resolvers/test/project.resolver.spec.ts
@@ -1,6 +1,13 @@
-import { NotFoundException, UnauthorizedException } from '@nestjs/common';
-import { Test, TestingModule } from '@nestjs/testing';
 import {
+  ConflictException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserInputError } from 'apollo-server-errors';
+import { createFeedbackInputFactory } from '../../../test/factories/feedback.factory';
+import {
+  addProjectMemberInputFactory,
   createProjectInputFactory,
   projectFactory,
   projectMemberFactory,
@@ -65,8 +72,10 @@ describe('ProjectResolver', () => {
   describe('projectMember', () => {
     it('should return project member', async () => {
       const project = projectFactory.build();
-      const projectMember = projectMemberFactory.buildList(10)
-      jest.spyOn(projectService, 'findMembers').mockResolvedValueOnce(projectMember);
+      const projectMember = projectMemberFactory.buildList(10);
+      jest
+        .spyOn(projectService, 'findMembers')
+        .mockResolvedValueOnce(projectMember);
       const result = await projectResolver.projectMember(project);
       expect(result).toBe(projectMember);
     });
@@ -74,7 +83,96 @@ describe('ProjectResolver', () => {
       const project = projectFactory.build();
       jest.spyOn(projectService, 'findMembers').mockResolvedValueOnce([]);
       const result = await projectResolver.projectMember(project);
-      expect(result).toHaveLength(0)
+      expect(result).toHaveLength(0);
+    });
+  });
+  describe('createProject', () => {
+    it('should create project', async () => {
+      const user = userFactory.build();
+      const project = projectFactory.build();
+      const createProjectInput = createProjectInputFactory.build();
+      jest.spyOn(projectService, 'create').mockResolvedValueOnce(project);
+      const result = await projectResolver.createProject(
+        user,
+        createProjectInput,
+      );
+      expect(result).toBe(project);
+    });
+  });
+  describe('addProjectMember', () => {
+    it('should add user to project', async () => {
+      const member = userFactory.build();
+      const user = userFactory.build();
+      const projectMember = projectMemberFactory.buildList(10, {
+        user: member,
+      });
+      const project = projectFactory.build({});
+      const addProjectMemberInput = addProjectMemberInputFactory.build({
+        projectId: project.id,
+        memeberEmail: member.email,
+      });
+      jest.spyOn(projectService, 'findOne').mockResolvedValueOnce(project);
+      jest
+        .spyOn(userService, 'adminOfProjectWithId')
+        .mockResolvedValueOnce(true);
+      jest.spyOn(userService, 'findOneByEmail').mockResolvedValueOnce(user);
+      jest
+        .spyOn(projectService, 'addMember')
+        .mockResolvedValueOnce(projectMember);
+
+      const result = await projectResolver.addProjectMember(
+        user,
+        addProjectMemberInput,
+      );
+      expect(result).toBe(projectMember);
+    });
+    it('should throw error when project does not exist', async () => {
+      const member = userFactory.build();
+      const user = userFactory.build();
+      const project = projectFactory.build({});
+      const addProjectMemberInput = addProjectMemberInputFactory.build({
+        projectId: project.id,
+        memeberEmail: member.email,
+      });
+      jest.spyOn(projectService, 'findOne').mockResolvedValueOnce(null);
+      await expect(async () => {
+        await projectResolver.addProjectMember(user, addProjectMemberInput);
+      }).rejects.toThrowError(NotFoundException);
+    });
+    it('should throw error when non admin user try add member', async () => {
+      const member = userFactory.build();
+      const user = userFactory.build();
+      const project = projectFactory.build({});
+      const addProjectMemberInput = addProjectMemberInputFactory.build({
+        projectId: project.id,
+        memeberEmail: member.email,
+      });
+      jest.spyOn(projectService, 'findOne').mockResolvedValueOnce(project);
+      jest
+        .spyOn(userService, 'adminOfProjectWithId')
+        .mockResolvedValueOnce(false);
+      jest.spyOn(userService, 'findOneByEmail').mockResolvedValueOnce(null);
+
+      await expect(async () => {
+        await projectResolver.addProjectMember(user, addProjectMemberInput);
+      }).rejects.toThrowError(UnauthorizedException);
+    });
+    it('should throw error when member does not have an account', async () => {
+      const member = userFactory.build();
+      const user = userFactory.build();
+      const project = projectFactory.build({});
+      const addProjectMemberInput = addProjectMemberInputFactory.build({
+        projectId: project.id,
+        memeberEmail: member.email,
+      });
+      jest.spyOn(projectService, 'findOne').mockResolvedValueOnce(project);
+      jest
+        .spyOn(userService, 'adminOfProjectWithId')
+        .mockResolvedValueOnce(true);
+
+      await expect(async () => {
+        await projectResolver.addProjectMember(user, addProjectMemberInput);
+      }).rejects.toThrowError(UserInputError);
     });
   });
 });
